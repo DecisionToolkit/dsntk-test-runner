7765c0221ad56cabf321bfc562e2822073c0c62b
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-09-20 18:18:31 +0200

    Updates. (#1)

--- src/dto.rs
+++ src/dto.rs
@@ -1,8 +1,13 @@
 //! # Data transfer objects for input and output values
 
 use crate::model::{Component, InputNode, List, Simple, Value};
+use iso8601_duration::Duration;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
+use std::process::exit;
+use std::str::FromStr;
 
 /// Data transfer object for an error.
 #[derive(Debug, Deserialize)]
@@ -68,41 +73,62 @@ pub struct SimpleDto {
   pub nil: bool,
 }
 
+/// Maximum allowed difference between compared decimals.
+const EPSILON: Decimal = dec!(0.000_000_006_7);
+
 impl PartialEq for SimpleDto {
   fn eq(&self, rhs: &Self) -> bool {
-    // if self.typ.is_some()
-    //   && rhs.typ.is_some()
-    //   && (self.typ.as_ref().unwrap() == "xsd:decimal" || self.typ.as_ref().unwrap() == "xsd:double")
-    //   && (rhs.typ.as_ref().unwrap() == "xsd:decimal" || rhs.typ.as_ref().unwrap() == "xsd:double")
-    //   && self.nil == rhs.nil
-    // {
-    //   return compare_decimals(self.text.clone(), rhs.text.clone());
-    // }
-    self.typ == rhs.typ && self.text == rhs.text && self.nil == rhs.nil
+    let mut result = self.typ == rhs.typ && self.text == rhs.text && self.nil == rhs.nil;
+    if !result {
+      // compare decimals with epsilon difference
+      if is_decimal(&self.typ) && is_decimal(&rhs.typ) && self.nil == rhs.nil {
+        let a = Decimal::from_str(&text(self)).unwrap();
+        let b = Decimal::from_str(&text(rhs)).unwrap();
+        let c = (a - b).abs();
+        result = c < EPSILON;
+        if !result {
+          println!("\n\nEncountered decimal comparison error");
+          println!(" a = {}", a);
+          println!(" b = {}", b);
+          println!(" c = {}", c);
+          println!(" ε = {}", EPSILON);
+          println!("compared decimals differ more than expected\n");
+          exit(0);
+        }
+        return result;
+      }
+      // compare durations
+      if is_duration(self) && is_duration(rhs) {
+        let a = text(self).parse::<Duration>().unwrap();
+        let b = text(rhs).parse::<Duration>().unwrap();
+        result = a == b;
+        return result;
+      }
+    }
+    result
   }
 }
 
-// ///
-// fn compare_decimals(actual: Option<String>, expected: Option<String>) -> bool {
-//   if let Some((actual_text, expected_text)) = actual.zip(expected.as_ref()) {
-//     if actual_text.starts_with(expected_text) {
-//       return true;
-//     }
-//     if actual_text.starts_with(&expected_text[..expected_text.len() - 1]) {
-//       //TODO report warning 1
-//       return true;
-//     }
-//     if actual_text.starts_with(&expected_text[..expected_text.len() - 2]) {
-//       //TODO report warning 2
-//       return true;
-//     }
-//     if actual_text.starts_with(&expected_text[..expected_text.len() - 3]) {
-//       //TODO report warning 3
-//       return true;
-//     }
-//   }
-//   false
-// }
+fn text(value: &SimpleDto) -> String {
+  value.text.as_ref().unwrap().clone()
+}
+
+fn is_decimal(opt_type: &Option<String>) -> bool {
+  let Some(typ) = opt_type else {
+    return false;
+  };
+  ["xsd:decimal", "xsd:double"].contains(&typ.as_str())
+}
+
+fn is_duration(value: &SimpleDto) -> bool {
+  if value.nil {
+    return false;
+  }
+  let Some(typ) = &value.typ else {
+    return false;
+  };
+  ["xsd:duration"].contains(&typ.as_str())
+}
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct ComponentDto {
